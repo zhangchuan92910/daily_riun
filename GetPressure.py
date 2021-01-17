8df525bbe3380ccb3876318b21bbca8293477850
zhangchuan92910 <64056472+zhangchuan92910@users.noreply.github.com>
2021-01-16 19:40:18 -0800

    Add files via upload

---
+++ GetPressure.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from requests_html import HTMLSession
+
+
+# In[106]:
+
+
+import time
+
+
+# In[2]:
+
+
+session = HTMLSession()
+
+
+# In[3]:
+
+
+url = 'https://weather.gc.ca/city/pages/bc-96_metric_e.html'
+
+
+# In[4]:
+
+
+r = session.get(url)
+
+
+# In[102]:
+
+
+sel = 'body > main > div > div#mainContent > section:nth-child(1) > details.panel.panel-default.hidden-xs > div.row.no-gutters.wb-eqht.hidden-print > div.col-sm-10.text-center > div > div:nth-child(1) > dl > dd.mrgn-bttm-0.wxo-metric-hide'
+
+
+# In[103]:
+
+
+result0 = r.html.find(sel)
+
+sel1 = 'body > main > div > div#mainContent > section:nth-child(1) > details.panel.panel-default.visible-xs.wxo-obs.mrgn-bttm-md.mrgn-tp-sm > details > dl > dd:nth-child(2)'
+
+result1 = r.html.find(sel1)
+# In[112]:
+
+
+
+record = open('record.csv','a')
+record.write(','.join(result1[0].text.split())+','+result0[0].text+'\n')
+record.close()
+
+
+
+
